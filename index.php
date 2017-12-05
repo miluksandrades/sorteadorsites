86a3dc2a34b2f0e0efbc333628250371f025be49
miluksandrades <luksandrades18@gmail.com>
2017-12-05 03:54:58 -0200

    update

--- index.php
+++ index.php
@@ -33,18 +33,39 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     </head>
     <body>
-        <div class="container-fluid text-white"style="text-align: center">
+        <div class="container-fluid"style="text-align: center">
+            <br/>
+            <div class="container-fluid col-md-4 col-sm-8 col-xl-2 justify-content-center" style="border-radius: 5px; text-align: center;">
+                <form>
+                    <div class="form-group">
+                        <label class="control-label">De:</label>
+                        <input type="number" id="inicio" class="form-control"/>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label">Até:</label>
+                        <input type="number" id="fim" class="form-control"/>
+                    </div>
+                    <button class="btn btn-primary" id="enviar">Carregar</button>
+                </form>
+            </div>
+            
             <br/>
             <div class="container-fluid col-md-4 col-sm-8 col-xl-2 bg-success justify-content-center" style="border-radius: 5px; text-align: center;">
-                <p class="lead" style="font-size: 90px"><b><?php echo rand(1, 400); ?></b></p>
+                <p class="lead text-white" id="numeroSorteado" style="font-size: 90px"></p>
                 <!--<p class="lead" style="font-size: 90px"><b>GO</b><i class="fa fa-arrow-down"></i></p>-->
             </div>
-            <button onclick="carregar()" class="col-md-4 col-sm-8 col-xl-2 btn btn-lg btn-outline-primary">Novo Sorteio</button>
         </div>
         <script>
-            function carregar(){
-                location.reload();
-            }
+
+            var inicio = document.getElementById("inicio").value;
+            var fim = document.getElementById("fim").value;
+
+            document.getElementById("enviar").onclick = function(e){
+
+                e.preventDefault();
+                
+                document.getElementById("numeroSorteado").innerHTML="<b>"+Math.random()*fim+"</b>";
+            };
         </script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
