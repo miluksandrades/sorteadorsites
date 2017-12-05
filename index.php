1e24bcae74920e08ebc09d43341317c894763411
miluksandrades <luksandrades18@gmail.com>
2017-12-05 11:29:48 -0200

    update do gerador

--- index.php
+++ index.php
@@ -1,31 +1,3 @@
-<?php
-
-    /*$host = 'localhost';
-    $user = 'root';
-    $database = 'sorteador';
-
-    $con = mysql_connect($host, $user) or die(mysql_error());
-
-    mysql_select_db($database, $con);
-
-    $q = "SELECT * FROM numeros";
-
-    $query = mysql_query($q) or die(mysql_error());
-
-    $total = mysql_num_rows($query);
-
-    $numero = rand(1, $total);
-
-    $q = "SELECT * FROM numeros order by id ASC limit ".$numero.",1";
-
-    $query = mysql_query($q);
-
-    $resultado = mysql_fetch_array($query);*/
-
-    //echo 'Numero sorteado: '.$resultado["numero"];
-
-?>
-
 <html>
     <head>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
@@ -35,37 +7,58 @@
     <body>
         <div class="container-fluid"style="text-align: center">
             <br/>
-            <div class="container-fluid col-md-4 col-sm-8 col-xl-2 justify-content-center" style="border-radius: 5px; text-align: center;">
-                <form>
-                    <div class="form-group">
-                        <label class="control-label">De:</label>
-                        <input type="number" id="inicio" class="form-control"/>
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label">Até:</label>
-                        <input type="number" id="fim" class="form-control"/>
-                    </div>
-                    <button class="btn btn-primary" id="enviar">Carregar</button>
-                </form>
-            </div>
+            <!--<div class="container-fluid col-md-4 col-sm-8 col-xl-2 justify-content-center" style="border-radius: 5px; text-align: center;">
+                <form><div class="form-group"><label class="control-label">De:</label><input type="number" id="inicio" class="form-control"/></div>
+                    <div class="form-group"><label class="control-label">Até:</label><input type="number" id="fim" class="form-control"/></div>
+                    <button class="btn btn-outline-primary" id="enviar">Novo Número</button></form>
+            </div>-->
             
             <br/>
             <div class="container-fluid col-md-4 col-sm-8 col-xl-2 bg-success justify-content-center" style="border-radius: 5px; text-align: center;">
-                <p class="lead text-white" id="numeroSorteado" style="font-size: 90px"></p>
-                <!--<p class="lead" style="font-size: 90px"><b>GO</b><i class="fa fa-arrow-down"></i></p>-->
+                <!--<p class="lead text-white" id="numeroSorteado" style="font-size: 90px"></p>-->
+                <p class="lead text-white" id="numeroSorteado"  style="font-size: 90px"><b>GO</b><i class="fa fa-arrow-down"></i></p>
             </div>
+            <button class="col-md-4 col-sm-8 col-xl-2 btn btn-lg btn-outline-primary" onclick="gerar()">Novo Número</button>
         </div>
         <script>
+            function gerar(){
+                var numeros = [1,      2,      3,      4,      5,      6,      7,      8,      9,      10,     11,     12,     13,     14,     15,
+                            16,     17,     18,     19,     20,     21,     22,     23,     24,     25,     26,     27,     28,     29,     30,
+                            31,     32,     33,     34,     35,     36,     37,     38,     39,     40,     41,     42,     43,     44,     45,
+                            46,     47,     48,     49,     50,     51,     52,     53,     54,     55,     56,     57,     58,     59,     60,
+                            61,     62,     63,     64,     65,     66,     67,     68,     69,     70,     71,     72,     73,     74,     75,
+                            76,     77,     78,     79,     80,     81,     82,     83,     84,     85,     86,     87,     88,     89,     90,
+                            91,     92,     93,     94,     95,     96,     97,     98,     99,     100,    101,    102,    103,    104,    105,
+                            106,    107,    108,    109,    110,    111,    112,    113,    114,    115,    116,    117,    118,    119,    120,
+                            121,    122,    123,    124,    125,    126,    127,    128,    129,    130,    131,    132,    133,    134,    135,
+                            136,    137,    138,    139,    140,    141,    142,    143,    144,    145,    146,    147,    148,    149,    150,
+                            151,    152,    153,    154,    155,    156,    157,    158,    159,    160,    161,    162,    163,    164,    165,
+                            166,    167,    168,    169,    170,    171,    172,    173,    174,    175,    176,    177,    178,    179,    180,
+                            181,    182,    183,    184,    185,    186,    187,    188,    189,    190,    191,    192,    193,    194,    195,
+                            196,    197,    198,    199,    200,    201,    202,    203,    204,    205,    206,    207,    208,    209,    210,
+                            211,    212,    213,    214,    215,    216,    217,    218,    219,    220,    221,    222,    223,    224,    225,
+                            226,    227,    228,    229,    230,    231,    232,    233,    234,    235,    236,    237,    238,    239,    240,
+                            241,    242,    243,    244,    245,    246,    247,    248,    249,    250,    251,    252,    253,    254,    255,
+                            256,    257,    258,    259,    260,    261,    262,    263,    264,    265,    266,    267,    268,    269,    270,
+                            271,    272,    273,    274,    275,    276,    277,    278,    279,    280,    281,    282,    283,    284,    285,
+                            286,    287,    288,    289,    290,    291,    292,    293,    294,    295,    296,    297,    298,    299,    300,
+                            301,    302,    303,    304,    305,    306,    307,    308,    309,    310,    311,    312,    313,    314,    315,
+                            316,    317,    318,    319,    320,    321,    322,    323,    324,    325,    326,    327,    328,    329,    330,
+                            331,    332,    333,    334,    335,    336,    337,    338,    339,    340,    341,    342,    343,    344,    345,
+                            346,    347,    348,    349,    350,    351,    352,    353,    354,    355,    356,    357,    358,    359,    360,
+                            361,    362,    363,    364,    365,    366,    367,    368,    369,    370,    371,    372,    373,    374,    375,
+                            376,    377,    378,    379,    380,    381,    382,    383,    384,    385,    386,    387,    388,    389,    390,
+                            391,    392,    393,    394,    395,    396,    397,    398,    399,    400];
 
-            var inicio = document.getElementById("inicio").value;
-            var fim = document.getElementById("fim").value;
+                var numeroSorteado = Math.floor(Math.random(numeros)*290+1);
 
-            document.getElementById("enviar").onclick = function(e){
+                //if(numeroSorteado.length == 1)
+                    //numeroSorteado += "0"+numeroSorteado;
 
-                e.preventDefault();
+                document.getElementById("numeroSorteado").innerHTML="<b>"+numeroSorteado+"</b>";
                 
-                document.getElementById("numeroSorteado").innerHTML="<b>"+Math.random()*fim+"</b>";
-            };
+
+            }
         </script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
